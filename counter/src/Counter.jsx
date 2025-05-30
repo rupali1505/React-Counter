7238f2116a24c402656b9f42de716f7a9fdc0090
rupali dudhal <rupalidudhal95@gmail.com>
2025-05-30 23:27:50 +0530

    changes done

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -7,7 +7,7 @@ export default function Counter(){
             <h2>Counter App</h2>
             <p>Count: {value}</p>
             <button onClick={()=> setValue((c)=>c+1)}>Increment</button>   
-            <button onClick={()=> setValue(()=>0-1)}>Decrement</button>     
+            <button onClick={()=> setValue((c)=>c-1)}>Decrement</button>     
        </div>
     )
 }
