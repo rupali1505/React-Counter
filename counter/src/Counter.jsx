4d8d5305a6dab3effe500e95f94b5871a09948f7
rupali dudhal <rupalidudhal95@gmail.com>
2025-05-30 23:16:04 +0530

    counter app

---
+++ counter/src/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function Counter(){
+    const [value, setValue] = useState(0);
+    return(
+        <div>
+            <h2>Counter App</h2>
+            <p>Count : {value}</p>
+            <button onClick={()=> setValue((c)=>c+1)}>Increment</button>   
+            <button onClick={()=> setValue((c)=>c-1)}>Decrement</button>     
+       </div>
+    )
+}
